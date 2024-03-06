bd8a0a9581ad845ec4e5c776c9a913ff5815e0a3
DDDDKO <to1583@naver.com>
2024-03-06 22:24:41 +0900

    2024-03-06 22:30

---
+++ Practice problem.java
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/161989?language=java
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        return answer;
+    }
+}
